6dc8dea3590345136cd8ca1d5530943e26511bde
张为杰 <zhangwj0520@hotmail.com>
2024-07-04 16:13:39 +0800

    openapi-ts

---
+++ openapi-ts.config.ts
@@ -0,0 +1,18 @@
+/** @type {import('@hey-api/openapi-ts').UserConfig} */
+export default {
+  input: 'http://127.0.0.1:8910/openapi.json',
+  output: {
+      path:"app/client",
+      format: 'biome' 
+  },
+  types: {
+    enums: 'javascript',
+  },
+  client:'fetch',
+  schemas: {
+    type: 'json'
+  },
+  services: {
+    asClass: true,
+  },
+};
